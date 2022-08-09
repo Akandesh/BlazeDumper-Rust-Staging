3c815766770a66e504aca7357d12197e768e695a
Akandesh <Akandessh@live.com>
2022-08-09 18:58:25 +0200

    Update: Tue Aug  9 18:58:25 WEDT 2022

--- rust.h
+++ rust.h
@@ -2,9 +2,9 @@
 #include <cstdint>
 
 namespace blazedumper {
-    constexpr ::std::ptrdiff_t BaseEntity_TypeInfo = 0x32708C0;
-    constexpr ::std::ptrdiff_t MainCamera_TypeInfo = 0x3272508;
-    constexpr ::std::ptrdiff_t Facepunch_Input_TypeInfo = 0x3282CB8;
+    constexpr ::std::ptrdiff_t BaseEntity_TypeInfo = 0x3272908;
+    constexpr ::std::ptrdiff_t MainCamera_TypeInfo = 0x3274540;
+    constexpr ::std::ptrdiff_t Facepunch_Input_TypeInfo = 0x3284D00;
     namespace BasePlayer {
         constexpr ::std::ptrdiff_t __menuOption_Climb = 0x240; // Option
         constexpr ::std::ptrdiff_t __menuOption_Drink = 0x298; // Option
