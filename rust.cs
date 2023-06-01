78c2bc371266ee5599507ce4b06503f6668486e0
Akandesh <Akandessh@live.com>
2023-06-01 12:12:59 +0200

    Update: Thu Jun  1 12:12:59 WEDT 2023

--- rust.cs
+++ rust.cs
@@ -6,11 +6,11 @@ namespace blazedumper {
     }
     public static class Facepunch_Input_TypeInfo
     {
-    public const Int32 offset = 0x2FC10F0;
+    public const Int32 offset = 0x2FC10E8;
     }
     public static class MainCamera_TypeInfo
     {
-    public const Int32 offset = 0x2FD2730;
+    public const Int32 offset = 0x2FD2728;
     }
     public static class System_Collections_Generic_List_BaseGameMode_TypeInfo
     {
