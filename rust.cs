8ed3f0ba21edd53b41dac2b8561cff17133898f9
Akandesh <Akandessh@live.com>
2023-10-24 10:08:22 +0200

    Update: Tue Oct 24 10:08:22 WEDT 2023

--- rust.cs
+++ rust.cs
@@ -2,23 +2,23 @@ using System;
 namespace blazedumper {
     public static class BaseEntity_TypeInfo
     {
-    public const Int32 offset = 0x3380570;
+    public const Int32 offset = 0x3380590;
     }
     public static class Facepunch_Input_TypeInfo
     {
-    public const Int32 offset = 0x33D9128;
+    public const Int32 offset = 0x33D9148;
     }
     public static class MainCamera_TypeInfo
     {
-    public const Int32 offset = 0x33EE190;
+    public const Int32 offset = 0x33EE1F8;
     }
     public static class System_Collections_Generic_List_BaseGameMode_TypeInfo
     {
-    public const Int32 offset = 0x33BFB30;
+    public const Int32 offset = 0x33BFB50;
     }
     public static class BaseGameMode_TypeInfo
     {
-    public const Int32 offset = 0x33807F8;
+    public const Int32 offset = 0x3380820;
     }
     public static class BasePlayer
     {
--- rust.h
+++ rust.h
@@ -2,11 +2,11 @@
 #include <cstdint>
 
 namespace blazedumper {
-    constexpr ::std::ptrdiff_t BaseEntity_TypeInfo = 0x3380570;
-    constexpr ::std::ptrdiff_t Facepunch_Input_TypeInfo = 0x33D9128;
-    constexpr ::std::ptrdiff_t MainCamera_TypeInfo = 0x33EE190;
-    constexpr ::std::ptrdiff_t System_Collections_Generic_List_BaseGameMode_TypeInfo = 0x33BFB30;
-    constexpr ::std::ptrdiff_t BaseGameMode_TypeInfo = 0x33807F8;
+    constexpr ::std::ptrdiff_t BaseEntity_TypeInfo = 0x3380590;
+    constexpr ::std::ptrdiff_t Facepunch_Input_TypeInfo = 0x33D9148;
+    constexpr ::std::ptrdiff_t MainCamera_TypeInfo = 0x33EE1F8;
+    constexpr ::std::ptrdiff_t System_Collections_Generic_List_BaseGameMode_TypeInfo = 0x33BFB50;
+    constexpr ::std::ptrdiff_t BaseGameMode_TypeInfo = 0x3380820;
     namespace BasePlayer {
         constexpr ::std::ptrdiff_t __menuOption_Climb = 0x270; // Option
         constexpr ::std::ptrdiff_t __menuOption_Drink = 0x2F0; // Option
