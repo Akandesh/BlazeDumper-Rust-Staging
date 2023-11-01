5428b151b3baf7960005834da2f1735b0e473625
Akandesh <Akandessh@live.com>
2023-11-01 12:49:17 +0100

    Update: Wed Nov  1 12:49:17 WEST 2023

--- rust.h
+++ rust.h
@@ -2,11 +2,11 @@
 #include <cstdint>
 
 namespace blazedumper {
-    constexpr ::std::ptrdiff_t BaseEntity_TypeInfo = 0x339E598;
-    constexpr ::std::ptrdiff_t Facepunch_Input_TypeInfo = 0x33F7190;
-    constexpr ::std::ptrdiff_t MainCamera_TypeInfo = 0x340C418;
-    constexpr ::std::ptrdiff_t System_Collections_Generic_List_BaseGameMode_TypeInfo = 0x33DD6A0;
-    constexpr ::std::ptrdiff_t BaseGameMode_TypeInfo = 0x339E830;
+    constexpr ::std::ptrdiff_t BaseEntity_TypeInfo = 0x339E5A8;
+    constexpr ::std::ptrdiff_t Facepunch_Input_TypeInfo = 0x33F71A0;
+    constexpr ::std::ptrdiff_t MainCamera_TypeInfo = 0x340C428;
+    constexpr ::std::ptrdiff_t System_Collections_Generic_List_BaseGameMode_TypeInfo = 0x33DD6B0;
+    constexpr ::std::ptrdiff_t BaseGameMode_TypeInfo = 0x339E840;
     namespace BasePlayer {
         constexpr ::std::ptrdiff_t __menuOption_Climb = 0x270; // Option
         constexpr ::std::ptrdiff_t __menuOption_Drink = 0x2F0; // Option
