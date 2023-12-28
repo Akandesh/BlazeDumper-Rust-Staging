69fcc5e05e0d39cf4dcd2f895b1cb05578dc11aa
Akandesh <Akandessh@live.com>
2023-12-28 14:17:27 +0100

    Update: Thu Dec 28 14:17:27 WEST 2023

--- rust.h
+++ rust.h
@@ -2,11 +2,11 @@
 #include <cstdint>
 
 namespace blazedumper {
-    constexpr ::std::ptrdiff_t BaseEntity_TypeInfo = 0x347CF60;
-    constexpr ::std::ptrdiff_t Facepunch_Input_TypeInfo = 0x34D6548;
-    constexpr ::std::ptrdiff_t MainCamera_TypeInfo = 0x34EBC50;
-    constexpr ::std::ptrdiff_t System_Collections_Generic_List_BaseGameMode_TypeInfo = 0x34BC218;
-    constexpr ::std::ptrdiff_t BaseGameMode_TypeInfo = 0x347D218;
+    constexpr ::std::ptrdiff_t BaseEntity_TypeInfo = 0x347DFC0;
+    constexpr ::std::ptrdiff_t Facepunch_Input_TypeInfo = 0x34D7640;
+    constexpr ::std::ptrdiff_t MainCamera_TypeInfo = 0x34ECD48;
+    constexpr ::std::ptrdiff_t System_Collections_Generic_List_BaseGameMode_TypeInfo = 0x34BD318;
+    constexpr ::std::ptrdiff_t BaseGameMode_TypeInfo = 0x347E278;
     namespace BasePlayer {
         constexpr ::std::ptrdiff_t __menuOption_Climb = 0x270; // Option
         constexpr ::std::ptrdiff_t __menuOption_Drink = 0x2F0; // Option
--- rust_inline.h
+++ rust_inline.h
@@ -2,11 +2,11 @@
 #include <cstdint>
 
 namespace blazedumper {
-    inline constexpr ::std::ptrdiff_t BaseEntity_TypeInfo = 0x347CF60;
-    inline constexpr ::std::ptrdiff_t Facepunch_Input_TypeInfo = 0x34D6548;
-    inline constexpr ::std::ptrdiff_t MainCamera_TypeInfo = 0x34EBC50;
-    inline constexpr ::std::ptrdiff_t System_Collections_Generic_List_BaseGameMode_TypeInfo = 0x34BC218;
-    inline constexpr ::std::ptrdiff_t BaseGameMode_TypeInfo = 0x347D218;
+    inline constexpr ::std::ptrdiff_t BaseEntity_TypeInfo = 0x347DFC0;
+    inline constexpr ::std::ptrdiff_t Facepunch_Input_TypeInfo = 0x34D7640;
+    inline constexpr ::std::ptrdiff_t MainCamera_TypeInfo = 0x34ECD48;
+    inline constexpr ::std::ptrdiff_t System_Collections_Generic_List_BaseGameMode_TypeInfo = 0x34BD318;
+    inline constexpr ::std::ptrdiff_t BaseGameMode_TypeInfo = 0x347E278;
     namespace BasePlayer {
         inline constexpr ::std::ptrdiff_t __menuOption_Climb = 0x270; // Option
         inline constexpr ::std::ptrdiff_t __menuOption_Drink = 0x2F0; // Option
