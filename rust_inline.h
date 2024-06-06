e2b864a4aec3feabeb95347dff193527c2cdb4ca
Akandesh <Akandessh@live.com>
2024-06-06 11:38:36 +0200

    Update: Thu Jun  6 11:38:36 WEDT 2024

--- rust_inline.h
+++ rust_inline.h
@@ -2,11 +2,11 @@
 #include <cstdint>
 
 namespace blazedumper {
-    inline constexpr ::std::ptrdiff_t BaseEntity_TypeInfo = 0x3C25B80;
-    inline constexpr ::std::ptrdiff_t Facepunch_Input_TypeInfo = 0x3BF9F10;
-    inline constexpr ::std::ptrdiff_t MainCamera_TypeInfo = 0x3C12298;
-    inline constexpr ::std::ptrdiff_t System_Collections_Generic_List_BaseGameMode_TypeInfo = 0x3C552C8;
-    inline constexpr ::std::ptrdiff_t BaseGameMode_TypeInfo = 0x3C25DE0;
+    inline constexpr ::std::ptrdiff_t BaseEntity_TypeInfo = 0x3C26CD0;
+    inline constexpr ::std::ptrdiff_t Facepunch_Input_TypeInfo = 0x3BFAFE0;
+    inline constexpr ::std::ptrdiff_t MainCamera_TypeInfo = 0x3C133B8;
+    inline constexpr ::std::ptrdiff_t System_Collections_Generic_List_BaseGameMode_TypeInfo = 0x3C56410;
+    inline constexpr ::std::ptrdiff_t BaseGameMode_TypeInfo = 0x3C26F30;
     namespace BasePlayer {
         inline constexpr ::std::ptrdiff_t __menuOption_Climb = 0x290; // Option
         inline constexpr ::std::ptrdiff_t __menuOption_Drink = 0x308; // Option
